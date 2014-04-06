1c35627898341a10ef39348c3310b45fdef034c9
kjsebastian <kiranjoysebastian@gmail.com>
2014-04-06 18:05:19 +0800

    testing travis

--- karma.conf.js
+++ karma.conf.js
@@ -19,8 +19,8 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'test/client/spec/controllers/*.js',
+      'test/server/thing/*.js'
     ],
 
     // list of files / patterns to exclude
