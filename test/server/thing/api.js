06c9177379dddb4f5c6aac07ed8ce301efc49209
kjsebastian <kiranjoysebastian@gmail.com>
2014-04-07 20:16:11 +0800

    redo test

--- test/server/thing/api.js
+++ test/server/thing/api.js
@@ -4,17 +4,15 @@ var should = require('should'),
     app = require('../../../server'),
     request = require('supertest');
 
-// describe('GET /api/awesomeThings', function() {
+describe('GET /api/all_bots', function() {
   
-//   it('should respond with JSON array', function(done) {
-//     request(app)
-//       .get('/api/awesomeThings')
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res) {
-//         if (err) return done(err);
-//         res.body.should.be.instanceof(Array);
-//         done();
-//       });
-//   });
-// });
+  it('should respond with JSON array', function(done) {
+    request(app)
+      .get('/api/all_bots')
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+});
